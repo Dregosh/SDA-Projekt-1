ae7e4525cc9b1173c7ce2e97322f7546347fb7a5
Rafal Piekos <rafal.peikos@gmail.com>
2020-08-30 15:31:58 +0200

    cleared App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,4 @@
-import controller.MainController;
-import model.Customer;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import service.CustomerService;
-import util.HibernateUtil;
-
-import java.util.List;
-
 public class App {
     public static void main(String[] args) {
-        CustomerService customerService = new CustomerService();
-        //customerService.initialCustomers();
-        List<Customer> customers = customerService.findByFullName("", "");
-        System.out.println("Test");
     }
 }
