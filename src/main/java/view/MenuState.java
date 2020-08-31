cbf7109c61873737c23c3f66505ee6f6d010d70d
Dregosh <matello@upcpoczta.pl>
2020-08-31 11:05:34 +0200

    Add implementation of AddNewProductToDB MenuOption

--- src/main/java/view/MenuState.java
+++ src/main/java/view/MenuState.java
@@ -1,13 +1,36 @@
 package view;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public abstract class MenuState {
-    protected Scanner in = new Scanner(System.in);
+    protected final Scanner in = new Scanner(System.in);
 
     abstract public void show();
 
-    public void reportNotImplentedInfo() {
+    public double requestNumberInput() {
+        while (true) {
+            try {
+                double input = in.nextDouble();
+                in.nextLine();
+                return input;
+            } catch (InputMismatchException e) {
+                System.out.println("Input must be a number");
+                in.nextLine();
+                System.out.print("> ");
+            }
+        }
+    }
+
+    public void reportNotImplented() {
         System.out.println("Option not implemented yet.");
+    }
+
+    public void reportOperationSuccessful() {
+        System.out.println("Operation successful");
+    }
+
+    public void reportOperationCancelled() {
+        System.out.println("Operation cancelled");
     }
 }
--- src/main/java/view/ProductMenuState.java
+++ src/main/java/view/ProductMenuState.java
@@ -7,7 +7,11 @@ import model.ProductType;
 import java.util.Objects;
 
 public class ProductMenuState extends MenuState {
-    private ProductController productController;
+    private final ProductController productController;
+    public static final boolean BLANK_INPUT_ALLOWED = true;
+    public static final boolean BLANK_INPUT_NOT_ALLOWED = false;
+    public static final boolean ZERO_ALLOWED = true;
+    public static final boolean ZERO_NOT_ALLOWED = false;
 
     public ProductMenuState(ProductController productController) {
         this.productController = productController;
@@ -21,7 +25,7 @@ public class ProductMenuState extends MenuState {
         System.out.println("(4) To Product Browser Menu...");
         System.out.println("(0) Return to previous menu");
         System.out.print("> ");
-        int input = in.nextInt();
+        int input = (int) requestNumberInput();
         switch (input) {
             case 1:
                 addNewProductOption();
@@ -36,46 +40,31 @@ public class ProductMenuState extends MenuState {
                 toProductBrowserMenuOption();
                 break;
             case 0:
-            default:
                 returnToPreviousMenuOption();
+                break;
+            default:
+                System.out.println("Invalid choice");
         }
     }
 
     private void addNewProductOption() {
-        System.out.print("Enter product name: ");
-        String productName = in.nextLine();
-        for (int i = 0; i < ProductType.values().length; i++) {
-            System.out.print("(" + (i + 1) + ") " + ProductType.values()[i] + " ");
-        }
-        System.out.println();
-        System.out.print("Enter product type number: ");
-        int prodTypeNo = in.nextInt();
-        ProductType prodType = ProductType.values()[prodTypeNo - 1];
-        System.out.print("Enter product price: ");
-        double prodPrice = in.nextDouble();
-        System.out.print("Enter amount of products in stock: ");
-        int prodAmount = in.nextInt();
-        Product product = new Product(productName, prodType, prodPrice, prodAmount);
+        String name = defineProductName(BLANK_INPUT_NOT_ALLOWED);
+        showProductTypes();
+        int typeNumber = defineLegitProductTypeNumber(ZERO_NOT_ALLOWED);
+        ProductType type = ProductType.values()[typeNumber - 1];
+        double price = defineProductPrice();
+        int amount = defineAmountInStock();
+        Product product = new Product(name, type, price, amount);
         productController.addNewProductToDB(product);
+        System.out.println("New product added to the DataBase");
     }
 
     private void updateExistingProductOption() {
-        productSelectionProcess();
-    }
-
-    private void productSelectionProcess() {
-        System.out.print("Enter product ID or 0 to cancel: ");
-        long input = in.nextLong();
-        Product product = productController.findProductById(input);
-        if (Objects.isNull(product)) {
-            System.out.println("Product not found");
-        } else {
-            System.out.println("Found: " + product);
-        }
+        defineProductForContext();
     }
 
     private void removeProductOption() {
-        reportNotImplentedInfo();
+        reportNotImplented();
     }
 
     private void toProductBrowserMenuOption() {
@@ -84,5 +73,88 @@ public class ProductMenuState extends MenuState {
 
     protected void returnToPreviousMenuOption() {
         productController.returnToPreviousMenu();
+    }
+
+    private void defineProductForContext() {
+        System.out.print("Enter product ID or 0 to cancel: ");
+        int input;
+        do {
+            input = (int) requestNumberInput();
+            Product product = productController.findProductById(input);
+            if (Objects.isNull(product)) {
+                System.out.println("Product not found");
+            } else {
+                System.out.println("Found: " + product);
+            }
+        } while (input != 0);
+    }
+
+    private String defineProductName(boolean allowBlank) {
+        String productName = "";
+        while (productName.isBlank()) {
+            System.out.print("Enter product name: ");
+            productName = in.nextLine();
+            if (productName.isBlank() && allowBlank) {
+                return productName;
+            } else if (productName.isBlank()) {
+                System.out.println("Name cannot be empty");
+            }
+        }
+        return productName;
+    }
+
+    private int defineLegitProductTypeNumber(boolean allowZero) {
+        int prodTypeNo;
+        boolean legitInput;
+        do {
+            System.out.print("Enter product type number: ");
+            prodTypeNo = (int) requestNumberInput();
+            legitInput = verifyInputProductType(allowZero, prodTypeNo);
+            if (!legitInput) {
+                System.out.println("Invalid number. Try again.");
+            }
+        } while (!legitInput);
+        return prodTypeNo;
+    }
+
+    private double defineProductPrice() {
+        boolean legitInput;
+        double prodPrice;
+        do {
+            System.out.print("Enter product price: ");
+            prodPrice = requestNumberInput();
+            legitInput = (prodPrice > 0);
+            if (!legitInput) {
+                System.out.println("Price must be greater than 0");
+            }
+        } while (!legitInput);
+        return prodPrice;
+    }
+
+    private int defineAmountInStock() {
+        boolean legitInput;
+        int amount;
+        do {
+            System.out.print("Enter amount of products in stock: ");
+            amount = (int) requestNumberInput();
+            legitInput = (amount > 0);
+            if (!legitInput) {
+                System.out.println("Amount must be greater than 0");
+            }
+        } while (!legitInput);
+        return amount;
+    }
+
+    protected void showProductTypes() {
+        for (int i = 0; i < ProductType.values().length; i++) {
+            System.out.print("(" + (i + 1) + ") " + ProductType.values()[i] + " ");
+        }
+        System.out.println();
+    }
+
+    protected boolean verifyInputProductType(boolean allowZero, int number) {
+        int rangeMin = allowZero ? 0 : 1;
+        int rangeMax = ProductType.values().length;
+        return (number >= rangeMin && number <= rangeMax);
     }
 }
